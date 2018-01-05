de3c7a417e373a3307bb4ac67a11b5185e1ed762
satyarth Upadhyaya <satyarth7a@hotmail.com>
2018-01-05 15:39:18 +0545

    ethereum wallet implemented (#3)
    Ethereum wallet implementation

--- src/main/java/com/soriole/wallet/sqrapp/ethereum/Ethereum.java
+++ src/main/java/com/soriole/wallet/sqrapp/ethereum/Ethereum.java
@@ -1,21 +1,33 @@
 package com.soriole.wallet.sqrapp.ethereum;
 
+import java.security.SecureRandom;
+
+import org.ethereum.crypto.ECKey;
+import org.spongycastle.util.encoders.Hex;
+
 import com.soriole.wallet.sqrapp.CryptoCurrency;
 
 public class Ethereum implements CryptoCurrency{
+	private SecureRandom random = new SecureRandom();
+	private final int seedSize  = 32;
     @Override
     public byte[] newSeed() {
-        return new byte[0];
+    	
+		byte[] seed = new byte[seedSize];
+		random.nextBytes(seed);
+		return seed;
     }
 
     @Override
     public byte[] newPrivateKey() {
-        return new byte[0];
+    	ECKey key = new ECKey();
+    	return key.getPrivKeyBytes();
     }
 
     @Override
     public byte[] newPrivateKey(byte[] seed) {
-        return new byte[0];
+        ECKey key = ECKey.fromPrivate(seed);
+        return key.getPrivKeyBytes();
     }
 
     @Override
@@ -23,8 +35,18 @@ public class Ethereum implements CryptoCurrency{
         return new byte[0];
     }
 
+    //returns 65 byte public key i.e 64 bytes and one 0x00
     @Override
     public byte[] publicKey(byte[] privateKey) {
-        return new byte[0];
+        ECKey key = ECKey.fromPrivate(privateKey);
+        return key.getPubKey();
+        
+    }
+    
+    //gets 65 byte public key byte i.e 64 bytes and one 0x00
+    
+    public String getAddress(byte[] publicKey)
+    {
+    	return Hex.toHexString(ECKey.computeAddress(publicKey));
     }
 }
